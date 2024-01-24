3041a54125c5b5b1a3cecff050d276b8627f3c7d
Dhanar Adi Dewandaru <dhanar@accelbyte.net>
2024-01-24 08:23:04 +0700

    fix: auth interceptor when there is no token

--- pkg/common/auth_interceptor.go
+++ pkg/common/auth_interceptor.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2022 AccelByte Inc. All Rights Reserved.
+// Copyright (c) 2023 AccelByte Inc. All Rights Reserved.
 // This is licensed software from AccelByte Inc, for limitations
 // and restrictions contact your company contract manager.
 
@@ -6,74 +6,82 @@ package common
 
 import (
 	"context"
-	"fmt"
+	"os"
 	"strings"
 
 	"github.com/AccelByte/accelbyte-go-sdk/services-api/pkg/service/iam"
-	"github.com/pkg/errors"
-
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 )
 
 var OAuth *iam.OAuth20Service
 
 func UnaryAuthServerIntercept(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
-	if OAuth == nil {
-		return nil, errors.New("server token validator not set")
-	}
+	if !skipCheckAuthorizationMetadata(info.FullMethod) {
+		err := checkAuthorizationMetadata(ctx)
 
-	meta, found := metadata.FromIncomingContext(ctx)
-	if !found {
-		return nil, errors.New("metadata is missing")
-	}
-
-	if meta["authorization"] != nil {
-		authorization := meta["authorization"][0]
-		token := strings.TrimPrefix(authorization, "Bearer ")
-
-		claims, errParse := OAuth.ParseAccessTokenToClaims(token, false)
-		if errParse != nil {
-			return nil, errParse
-		}
-
-		err := OAuth.Validate(token, nil, &claims.ExtendNamespace, nil)
 		if err != nil {
 			return nil, err
 		}
-
-		fmt.Println("server: token validated.")
 	}
 
 	return handler(ctx, req)
 }
 
 func StreamAuthServerIntercept(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
-	if OAuth == nil {
-		return errors.New("server token validator not set")
-	}
+	if !skipCheckAuthorizationMetadata(info.FullMethod) {
+		err := checkAuthorizationMetadata(ss.Context())
 
-	meta, found := metadata.FromIncomingContext(ss.Context())
-	if !found {
-		return errors.New("metadata is missing")
-	}
-
-	if meta["authorization"] != nil {
-		authorization := meta["authorization"][0]
-		token := strings.TrimPrefix(authorization, "Bearer ")
-
-		claims, errParse := OAuth.ParseAccessTokenToClaims(token, false)
-		if errParse != nil {
-			return errParse
-		}
-
-		err := OAuth.Validate(token, nil, &claims.ExtendNamespace, nil)
 		if err != nil {
 			return err
 		}
-
-		fmt.Println("server: token validated.")
 	}
 
 	return handler(srv, ss)
+}
+
+func skipCheckAuthorizationMetadata(fullMethod string) bool {
+	if strings.HasPrefix(fullMethod, "/grpc.reflection.v1alpha.ServerReflection/") {
+		return true
+	}
+
+	if strings.HasPrefix(fullMethod, "/grpc.health.v1.Health/") {
+		return true
+	}
+
+	return false
+}
+
+func checkAuthorizationMetadata(ctx context.Context) error {
+	if OAuth == nil {
+		return status.Error(codes.Internal, "authorization token validator is not set")
+	}
+
+	meta, found := metadata.FromIncomingContext(ctx)
+
+	if !found {
+		return status.Error(codes.Unauthenticated, "metadata is missing")
+	}
+
+	if _, ok := meta["authorization"]; !ok {
+		return status.Error(codes.Unauthenticated, "authorization metadata is missing")
+	}
+
+	if len(meta["authorization"]) == 0 {
+		return status.Error(codes.Unauthenticated, "authorization metadata length is 0")
+	}
+
+	authorization := meta["authorization"][0]
+	token := strings.TrimPrefix(authorization, "Bearer ")
+	namespace := os.Getenv("AB_NAMESPACE")
+
+	err := OAuth.Validate(token, nil, &namespace, nil)
+
+	if err != nil {
+		return status.Error(codes.PermissionDenied, err.Error())
+	}
+
+	return nil
 }
